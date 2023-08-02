8024925c7b964eb1f7ac0c2a8d715858271db51e
Sofia Daouadji <sofia.daouadji@protonmail.com>
2023-08-02 14:11:17 -0400

    created 2 routes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './index.css'
 import Home from './pages/Home/index'
 import reportWebVitals from './reportWebVitals'
+import Survey from './pages/Survey/survey'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/survey" element={<Survey />}></Route>
+      </Routes>
+    </Router>
   </React.StrictMode>,
+  document.getElementById('root'),
 )
 
 // If you want to start measuring performance in your app, pass a function

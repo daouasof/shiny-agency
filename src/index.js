c80e0165074cc0ec299a5e4ad5d6a73f7aec8930
Sofia Daouadji <sofia.daouadji@protonmail.com>
2023-08-04 11:02:37 -0400

    styled the header and freelance cards

--- src/index.js
+++ src/index.js
@@ -9,11 +9,19 @@ import Header from './components/Header'
 import Error from './components/Error'
 import Results from './pages/Results'
 import Freelances from './pages/Freelances'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    * {
+      font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+`
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Router>
+      <GlobalStyle />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />

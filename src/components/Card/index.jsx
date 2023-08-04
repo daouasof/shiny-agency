59d8177ed799067e856f65a4fda8d81848c0770f
Sofia Daouadji <sofia.daouadji@protonmail.com>
2023-08-04 11:20:19 -0400

    added logo to header

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -35,7 +35,7 @@ function Card({ label, title, picture }) {
       style={{ display: 'flex', flexDirection: 'column', padding: 15 }}
     >
       <CardLabel>{label}</CardLabel>
-      <CardImage src={picture} alt="freelance" height={80} width={80} />
+      <CardImage src={picture} alt="freelance" />
       <span>{title}</span>
     </CardWrapper>
   )
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import colors from '../../utils/style/colors'
+import logo from '../../assets/dark-logo.png'
 
 const StyledLink = styled(Link)`
   padding: 15px;
@@ -12,15 +13,29 @@ const StyledLink = styled(Link)`
     `color: white; border-radius: 30px; background-color: ${colors.primary};`}
 `
 
+const StyledNav = styled.nav`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const StyledLogo = styled.img`
+  height: 50px;
+`
+
 function Header() {
   return (
-    <nav>
-      <StyledLink to="/">Accueil</StyledLink>
-      <StyledLink to="/freelances">Profils</StyledLink>
-      <StyledLink to="/survey/1" $isFullLink>
-        Faire le test
-      </StyledLink>
-    </nav>
+    <StyledNav>
+      <StyledLogo src={logo} alt="logo" />
+      <div>
+        <StyledLink to="/">Accueil</StyledLink>
+        <StyledLink to="/freelances">Profils</StyledLink>
+        <StyledLink to="/survey/1" $isFullLink>
+          Faire le test
+        </StyledLink>
+      </div>
+    </StyledNav>
   )
 }
 

bc1edf9ee16156b5eeb9fec6f050c4a32e6cff99
Sofia Daouadji <sofia.daouadji@protonmail.com>
2023-08-07 15:09:13 -0400

    styled error page

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,8 +1,28 @@
+import ErrorPicture from '../../assets/404.svg'
+import styled from 'styled-components'
+import colors from '../../utils/style/colors'
+
+const ErrorWrapper = styled.div`
+  background-color: ${colors.backgroundLight};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 30px;
+  padding: 50px;
+  color: ${colors.secondary};
+`
+
+const ErrorImage = styled.img`
+  width: 75%;
+`
+
 function Error() {
   return (
-    <div>
-      <h1>Oups, cette page n'existe pas</h1>
-    </div>
+    <ErrorWrapper>
+      <h2>Oups...</h2>
+      <ErrorImage src={ErrorPicture} alt="404" />
+      <h2>Il semblerait qu'il y ait un problème</h2>
+    </ErrorWrapper>
   )
 }
 
